967399a4650f84e6969b8333815babc740f30443
Vladislav Kuzmin <kuzmin.vv@yahoo.com>
2021-11-29 21:03:31 +0300

    Update test_app copy.py

--- test_app copy.py
+++ test_app copy.py
@@ -1,42 +1,45 @@
 import pandas as pd
-from sqlalchemy import create_engine, text
 
-db = create_engine('postgresql://postgres:Tftabbzgf1.@localhost/postgres')
+df = pd.read_excel('RC_F01_01_2019_T26_11_2021.xlsx', 0, header=0, index_col=None, na_values=["NA"])
 
-df = pd.read_sql(text("""SELECT "Час в дне", "День недели" FROM fes_out_db;"""), db)
+print('Import from database success!')
 
-df['Час в дне'] = df['Час в дне'].fillna(0)
-df['День недели'] = df['День недели'].fillna(0)
+df['month'] = pd.DatetimeIndex(df['DT']).month
+df['year'] = pd.DatetimeIndex(df['DT']).year
+display(df)
 
-print('Success!')
+import plotly.figure_factory as ff
+import numpy as np
 
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import plotly.express as px
+group_labels = ['2019', '2020', '3Q2021']
 
-animations = {
-    'Histogram Test': px.histogram(df, x="Час в дне", animation_frame="День недели"),
-}
+x1data = df.loc[df['year'] == 2019]
+x2data = df.loc[df['year'] == 2020]
+x3data = df.loc[df['year'] == 2021]
 
-app = dash.Dash(__name__)
+x1 = x1data['ruo']
+x2 = x2data['ruo']
+x3 = x3data['ruo']
 
-app.layout = html.Div([
-    html.P("Select an animation:"),
-    dcc.RadioItems(
-        id='selection',
-        options=[{'label': x, 'value': x} for x in animations],
-        value='Scatter'
-    ),
-    dcc.Graph(id="graph"),
-])
+rug_text_one = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y', 'z']
 
-@app.callback(
-    Output("graph", "figure"), 
-    [Input("selection", "value")])
-def display_animated_graph(s):
-    return animations[s]
+rug_text_two = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg', 'hh', 'ii', 'jj',
+                'kk', 'll', 'mm', 'nn', 'oo', 'pp', 'qq', 'rr', 'ss', 'tt',
+                'uu', 'vv', 'ww', 'xx', 'yy', 'zz']
 
-app.run_server(debug=True)
-print ('Server run!')
+rug_text_three = ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff', 'ggg', 'hhh', 'iii', 'jjj',
+                'kkk', 'lll', 'mmm', 'nnn', 'ooo', 'ppp', 'qqq', 'rrr', 'sss', 'ttt',
+                'uuu', 'vvv', 'www', 'xxx', 'yyy', 'zzz']
+
+rug_text = [rug_text_one, rug_text_two, rug_text_three] # for hover in rug plot
+colors = ['rgb(0, 0, 100)', 'rgb(0, 200, 200)', 'rgb(100, 0, 0)']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+    [x1, x2, x3], group_labels, bin_size=.2,
+    rug_text=rug_text, colors=colors)
+
+fig.update_layout(title_text='Customized Distplot')
+fig.show()
